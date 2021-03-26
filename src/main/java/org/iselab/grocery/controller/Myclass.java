6ad70d6d41ac8182a0e5a19a7f1fbe9455bf0599
Sami Hadouaj <samihadouaj@Samis-MacBook-Pro.local>
2021-03-26 13:45:11 +0100

    GROC-1 serie111

--- src/main/java/org/iselab/grocery/controller/Myclass.java
+++ src/main/java/org/iselab/grocery/controller/Myclass.java
@@ -17,4 +17,25 @@ public class Myclass {
             i++;
         }
     }
+
+    static float sumOfAP(float a, float d,
+                         int n)
+    {
+        float sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum = sum + a;
+            a = a + d;
+        }
+        return sum;
+    }
+
+    // Driver function
+    public static void serie1()
+    {
+        int n = 20;
+        float a = 2.5f, d = 1.5f;
+        System.out.println(sumOfAP(a, d, n));
+    }
+
 }
